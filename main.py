bcff26e7897965f00e246b9a1d1558000a89f3fb
siangi <siangi@gmx.ch>
2023-04-19 16:21:18 +0200

    added CORS support for when running the react app locally.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.searchModel import similaritySearchModel
 from database.searchModel import SEARCH_MODES
 
+
+BASE_GROUP_SIZE = 15
 #start with uvicorn main:app --reload
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,6 +28,6 @@ async def root():
 @app.get("/similars/{imageId}")
 async def similarFromExisting(imageId: int):
     searcher = similaritySearchModel()
-    similars = searcher.getImageListBySimilarity([SEARCH_MODES.PALETTE], 4, imageId)
+    similars = searcher.getImageListBySimilarity([SEARCH_MODES.PALETTE], 15, imageId)
     return {"message": similars}
 
